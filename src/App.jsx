5d46ebebc729b565f56f9968c8934105a10a657b
FadelYunus07 <fadelyunus07@gmail.com>
2023-12-09 16:05:24 +0700

    rubah app.jsx lagi

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import image from "./assets/image/Noobar color.svg";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import { Routes, Route } from "react-router-dom";
-import OriginalPage from "./pages/OriginalPage";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
 import SeriesPage from "./pages/SeriesPage";
 import DetailMovieComponent from "./components/DetailMovieComponent";
 import DetailTvComponent from "./components/DetailTvComponent";
 import SearchResultsPage from "./pages/SearchResultPage";
-import SearchComponent from "./components/SearchComponent";
 
 function App() {
-  const pathbase = "/noobar-react/";
+  const pathbase = "/noobar-react";
   return (
     <div>
       <HeaderComponent />
-      <Routes>
+      <Routes basename={pathbase}>
         <Route path="/search" element={<SearchResultsPage />} />
         <Route path={pathbase} element={<HomePage />} />
         <Route path={`${pathbase}/movie`} element={<MoviePage />} />
